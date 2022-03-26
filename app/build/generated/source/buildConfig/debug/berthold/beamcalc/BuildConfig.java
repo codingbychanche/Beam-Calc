0b3edf6773c290e565824ccc5e5603ee17bee9f0
Berthold <berthold.fritz@hotmail.com>
2022-03-26 20:11:14 +0100

    Improved UI

--- app/build/generated/source/buildConfig/debug/berthold/beamcalc/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/berthold/beamcalc/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String APPLICATION_ID = "berthold.beamcalc";
   public static final String BUILD_TYPE = "debug";
-  public static final int VERSION_CODE = 24;
-  public static final String VERSION_NAME = "2.4";
+  public static final int VERSION_CODE = 26;
+  public static final String VERSION_NAME = "2.6";
 }
